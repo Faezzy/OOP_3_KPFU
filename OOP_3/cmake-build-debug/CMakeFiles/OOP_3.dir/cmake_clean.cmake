e5b04fcbc2e397685b3784f8774e6d29b6ba059b
EMIL <curtaiku@student-21.school.ru>
2024-11-15 10:26:55 +0300

    in dev

---
+++ OOP_3/cmake-build-debug/CMakeFiles/OOP_3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_3.dir/main.cpp.o"
+  "CMakeFiles/OOP_3.dir/main.cpp.o.d"
+  "OOP_3"
+  "OOP_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
