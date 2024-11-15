603250309c220d632db5ff7ff9eed4d635e01478
EMIL <curtaiku@student-21.school.ru>
2024-11-15 11:03:40 +0300

    stage 2 develop

---
+++ OOP_3/Vector3d.h
@@ -0,0 +1,8 @@
+//
+// Created by faezzy4 on 15.11.24.
+//
+
+#ifndef OOP_3_VECTOR3D_H
+#define OOP_3_VECTOR3D_H
+
+#endif //OOP_3_VECTOR3D_H
--- OOP_3/main.cpp
+++ OOP_3/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
-
+//#include "Vector3d.h"
+using namespace std;
+class Vector3d{
+    double x;
+    double y;
+    double z;
+public:
+    Vector3d(double x, double y, double z): x(x), y(y), z(z){}
+    Vector3d Add(const Vector3d & Other){
+        Vector3d res;
+        res.x = this -> x + Other.x;
+        res.y = this -> y + Other.y;
+        res.z = this -> z + Other.z;
+        return res;
+    }
+    Vector3d operator+(const Vector3d & Other){
+        Vector3d res = Vector3d(this -> x + Other.x, this -> y + Other.y, this->z + Other.z);
+        return res;
+    }
+    double operator*(const Vector3d & Other){
+        return this -> x * Other.x + this-> y * Other.y + this -> z * Other.z;
+    }
+    Vector3d operator-(const Vector3d & Other){
+        Vector3d res = Vector3d(this -> x - Other.x, this -> x - Other.y, this -> y - Other.z);
+        return res;
+    }
+};
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << std::endl;
     return 0;
 }
