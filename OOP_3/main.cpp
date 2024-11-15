c7201f244463d19de889265b67ddb2ee05112e94
EMIL <curtaiku@student-21.school.ru>
2024-11-15 11:13:53 +0300

    dev st3

--- OOP_3/main.cpp
+++ OOP_3/main.cpp
@@ -1,32 +1,8 @@
 #include <iostream>
-//#include "Vector3d.h"
+#include "Vector3d.h"
 using namespace std;
-class Vector3d{
-    double x;
-    double y;
-    double z;
-public:
-    Vector3d(double x, double y, double z): x(x), y(y), z(z){}
-    Vector3d Add(const Vector3d & Other){
-        Vector3d res;
-        res.x = this -> x + Other.x;
-        res.y = this -> y + Other.y;
-        res.z = this -> z + Other.z;
-        return res;
-    }
-    Vector3d operator+(const Vector3d & Other){
-        Vector3d res = Vector3d(this -> x + Other.x, this -> y + Other.y, this->z + Other.z);
-        return res;
-    }
-    double operator*(const Vector3d & Other){
-        return this -> x * Other.x + this-> y * Other.y + this -> z * Other.z;
-    }
-    Vector3d operator-(const Vector3d & Other){
-        Vector3d res = Vector3d(this -> x - Other.x, this -> x - Other.y, this -> y - Other.z);
-        return res;
-    }
-};
 int main() {
-    cout << "Hello, World!" << std::endl;
+    Vector3d x;
+    Vector3d y;
     return 0;
 }
